4e01215652d3765394badf2f0c89fcc255371004
OlsonW <OlsonW@104-12-2017.bi.local>
2017-09-07 10:19:57 -0400

    github test

---
+++ Numerals/src/Numbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Numbers {
+	
+	
+	public static void main(String[] args) {
+		
+		String ask = "print a number";
+		String ans = "The sum is ";
+		int num = 0;
+		Scanner numbers = new Scanner(System.in);
+	
+		for (int i = 0; i < 2; i++) {
+		System.out.println(ask);
+		num = num + numbers.nextInt();
+		}
+		
+		System.out.println(ans + num);
+		
+		
+	}
+}
